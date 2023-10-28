fc301b20fb94ae660ae9bb71a20745980cbcb25b
RenzoSuganuma <discoverygworks2023@gmail.com>
2023-10-28 23:10:02 +0900

    Unity NavMeshAgent SetDestination と同じ機能っぽいのを実装

--- Assets/RS_LIB/LibraryCore/AI/AIComponentCluster/AIBrainComponent.cs
+++ Assets/RS_LIB/LibraryCore/AI/AIComponentCluster/AIBrainComponent.cs
@@ -40,6 +40,21 @@ namespace RSEngine
                 }
                 return path;
             }
+            /// <summary> 指定した始点と終点から距離を割り出して目的の距離の値に達したら true を返す。 </summary>
+            /// <param name="strt"></param>
+            /// <param name="end"></param>
+            /// <param name="limdis"></param>
+            /// <param name="limoffset"></param>
+            /// <returns></returns>
+            bool CheckInsideOfBorder(Vector3 strt, Vector3 end, float limdis, float limoffset) // detect based distance
+            {
+                float dx = end.x - strt.x;
+                float dy = strt.y - end.y;
+                float dz = end.z - strt.z;
+                float dd = dx * dx + dy * dy + dz * dz;
+                float lim = limdis * limdis;
+                return dd < lim + limoffset;
+            }
             /// <summary> レベルにターゲットが居るかの検索をかけたうえでターゲットをリターンする </summary>
             /// <returns></returns>
             (bool condition, List<Transform> targets) IsTargetInLevel()
@@ -61,6 +76,8 @@ namespace RSEngine
             private void FixedUpdate()
             {
                 _aiLimb.MoveToPoint(_path[_pathIndex], _moveSpeed);
+                var result = CheckInsideOfBorder(transform.position, _path[_pathIndex], 1, 1);
+                if (result) _pathIndex = (_pathIndex + 1 < _path.Count) ? _pathIndex + 1 : _pathIndex;
             }
             private void OnDrawGizmos()
             {
--- Assets/RS_LIB/LibraryCore/AI/AIComponentCluster/AILimbComponent.cs
+++ Assets/RS_LIB/LibraryCore/AI/AIComponentCluster/AILimbComponent.cs
@@ -9,21 +9,6 @@ namespace RSEngine
         /// <summary> 人体でいう四肢の機能を提供するクラス。移動を司る </summary>
         public class AILimbComponent : MonoBehaviour
         {
-            /// <summary> 指定した始点と終点から距離を割り出して目的の距離の値に達したら true を返す。 </summary>
-            /// <param name="strt"></param>
-            /// <param name="end"></param>
-            /// <param name="limdis"></param>
-            /// <param name="limoffset"></param>
-            /// <returns></returns>
-            bool CheckInsideOfBorder(Vector3 strt, Vector3 end, float limdis, float limoffset) // detect based distance
-            {
-                float dx = end.x - strt.x;
-                float dy = strt.y - end.y;
-                float dz = end.z - strt.z;
-                float dd = dx * dx + dy * dy + dz * dz;
-                float lim = limdis * limdis;
-                return dd < lim + limoffset;
-            }
             /// <summary> 渡された座標を目指して進む </summary>
             /// <param name="point"></param>
             /// <param name="speed"></param>
