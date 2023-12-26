a5ee48b4b04fadd6c9863f5d816eb6657ad5794b
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2023-12-26 14:12:01 +0900

    [upd] 入力バッファ実装

--- Assets/SuganumaDEV/SLib_Pack/SLib/LibraryCore/Inputs/InputBufferSample.cs
+++ Assets/SuganumaDEV/SLib_Pack/SLib/LibraryCore/Inputs/InputBufferSample.cs
@@ -3,6 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using SLib;
 using UnityEngine.InputSystem;
+using System.Linq;
+
+public enum DeviceInputType
+{
+    Move,
+    Look,
+    Fire,
+    Toggle,
+}
+
+public struct DeviceInputData
+{
+    InputAction.CallbackContext _context;
+    public InputAction.CallbackContext Context { get { return _context; } }
+    DeviceInputType _deviceInputType;
+    public DeviceInputType DeviceInputType { get { return _deviceInputType; } }
+
+    public DeviceInputData(InputAction.CallbackContext context, DeviceInputType type)
+    {
+        _context = context;
+        _deviceInputType = type;
+    }
+}
 
 public class InputBufferSample : MonoBehaviour
 {
@@ -12,6 +35,8 @@ public class InputBufferSample : MonoBehaviour
 
     bool _fire;
 
+    List<DeviceInputData> _inputHistory = new List<DeviceInputData>();
+
     private void Awake()
     {
         _inputBinder = GetComponent<PlayerInputBinder>();
@@ -19,14 +44,22 @@ public class InputBufferSample : MonoBehaviour
         _inputBinder.BindAxis("Player", "Move",
             (InputAction.CallbackContext context) =>
             {
-                _mInput = context.ReadValue<Vector2>();
+                //_mInput = context.ReadValue<Vector2>();
+
+                DeviceInputData deviceInput = new DeviceInputData(context, DeviceInputType.Move);
+
+                _inputHistory.Add(deviceInput);
             }
             , ActionInvokeFaze.Performed);
 
         _inputBinder.BindAxis("Player", "Look",
             (InputAction.CallbackContext context) =>
             {
-                _lInput = context.ReadValue<Vector2>();
+                //_lInput = context.ReadValue<Vector2>();
+
+                DeviceInputData deviceInput = new DeviceInputData(context, DeviceInputType.Look);
+
+                _inputHistory.Add(deviceInput);
             }, ActionInvokeFaze.Performed);
 
         _inputBinder.BindAction("Player", "Fire",
@@ -34,14 +67,20 @@ public class InputBufferSample : MonoBehaviour
             {
                 if (context.ReadValueAsButton())
                 {
-                    _fire = true;
+                    //_fire = true;
+                    DeviceInputData deviceInput = new DeviceInputData(context, DeviceInputType.Fire);
+
+                    _inputHistory.Add(deviceInput);
                 }
             },
             (InputAction.CallbackContext context) =>
             {
                 if (!context.ReadValueAsButton())
                 {
-                    _fire = false;
+                    //_fire = false;
+                    DeviceInputData deviceInput = new DeviceInputData(context, DeviceInputType.Fire);
+
+                    _inputHistory.Add(deviceInput);
                 }
             },
             (InputAction.CallbackContext context) => { }
@@ -50,6 +89,36 @@ public class InputBufferSample : MonoBehaviour
 
     private void Update()
     {
-        //Debug.Log($"Move : {_mInput.ToString()}, Look : {_lInput.ToString()}, Fire : {_fire}");
+
+        if (_inputHistory.Count > 0)
+        {
+            DeviceInputData InputData = _inputHistory[0];
+            _inputHistory.RemoveAt(0);
+
+            InputAction.CallbackContext context = InputData.Context;
+
+            Vector2 move = Vector2.zero;
+            Vector2 look = Vector2.zero;
+            bool fire = false;
+
+            switch (InputData.DeviceInputType)
+            {
+                case DeviceInputType.Move:
+                    move = context.ReadValue<Vector2>();
+                    break;
+                case DeviceInputType.Look:
+                    look = context.ReadValue<Vector2>();
+                    break;
+                case DeviceInputType.Fire:
+                    fire = context.ReadValueAsButton();
+                    break;
+                case DeviceInputType.Toggle:
+                    break;
+                default: break;
+            }
+
+            Debug.Log($"Move Input[{move.ToString()}] , Look Input[{look.ToString()}], Fire Input [{fire.ToString()}]");
+        }
+
     }
 }
