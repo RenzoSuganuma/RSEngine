c90ceae9e4888014db9eb117c09b55095f5bf70b
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2023-12-31 22:51:50 +0900

    no message

--- Assets/SLib_Pack/SLib/LibraryCore/SettingsManager/DisplaySettingsManager.cs
+++ Assets/SLib_Pack/SLib/LibraryCore/SettingsManager/DisplaySettingsManager.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+// 担当 菅沼
+// ディスプレイのデバイス名とリフレッシュレートの変更はできたっぽい ← ここまでは動作確認できている
 namespace SLib
 {
     public class DisplaySettingsManager : MonoBehaviour
     {
+        [SerializeField]
+            Text _text;
+
         public string GetResolution()
         {
             return $"{Screen.currentResolution.ToString()}";
@@ -15,6 +21,16 @@ namespace SLib
             List<DisplayInfo> list = new List<DisplayInfo>();
             Screen.GetDisplayLayout(list);
             return list[index].name;
+        }
+
+        public void SetDisplayResolutions(string resolution)
+        {
+            int width;
+            int height;
+            width = int.Parse(resolution.Split()[0]);
+            height = int.Parse(resolution.Split()[1]);
+
+            Screen.SetResolution(width, height, true, 60);
         }
 
         public int GetRefreshRate()
