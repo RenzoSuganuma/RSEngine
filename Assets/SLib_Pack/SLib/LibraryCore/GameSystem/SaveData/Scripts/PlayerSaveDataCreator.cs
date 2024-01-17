3c24244c8854cb1445870493379952114bea1f37
MSI_LapTop_RenzoSuganuma <103351677+RenzoSuganuma@users.noreply.github.com>
2024-01-17 18:28:52 +0900

    [] add library

--- Assets/SLib_Pack/SLib/LibraryCore/GameSystem/SaveData/Scripts/PlayerSaveDataCreator.cs
+++ Assets/SLib_Pack/SLib/LibraryCore/GameSystem/SaveData/Scripts/PlayerSaveDataCreator.cs
@@ -4,40 +4,40 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
-
-[Serializable]
-public class SaveDataTemplate
+//作成 菅沼
+namespace SLib
 {
-    public Transform _lastStandingTransform;       // Transform
-    public string _sceneName;                      // Scene Name
-}
-
-/// <summary> 渡されたフィールドの値をもとにScriptableObjectを生成、DataPath直下へ格納。 </summary>
-public class PlayerSaveDataCreator : SingletonBaseClass<PlayerSaveDataCreator>  // セーブデータの保存
-{
-    protected override void ToDoAtAwakeSingleton() { }
-
-    [SerializeField] Transform _tr;
-    [SerializeField] string _sceneName;
-
-    string _playerDataPath = Application.dataPath + "/PlayerSavedData.json";
-
-    public void SaveDatas()
+    namespace Systems
     {
-        SavePlayerData(_tr, _sceneName);
-    }
+        [Serializable]
+        public class SaveDataTemplate
+        {
+            public Vector3 _lastStandingPosition;       // Pos
+            public Quaternion _lastStandingRotation;       // Rot
+            public string _sceneName;                      // Scene Name
+        }
 
-    public void SavePlayerData(Transform playerStandingTransform, string sceneName)
-    {
-        SaveDataTemplate template = new SaveDataTemplate();
-        template._lastStandingTransform = playerStandingTransform;
-        template._sceneName = sceneName;
+        /// <summary> 渡されたフィールドの値をもとにScriptableObjectを生成、DataPath直下へ格納。 </summary>
+        public class PlayerSaveDataCreator : SingletonBaseClass<PlayerSaveDataCreator>  // セーブデータの保存
+        {
+            protected override void ToDoAtAwakeSingleton() { }
 
-        string jsonStr = JsonUtility.ToJson(template);
+            string _playerDataPath = Application.dataPath + "/PlayerSavedData.json";
 
-        StreamWriter sw = new StreamWriter(_playerDataPath, false);
-        sw.WriteLine(jsonStr);
-        sw.Flush();
-        sw.Close();
+            public void SavePlayerData(Transform playerStandingTransform, string sceneName)
+            {
+                SaveDataTemplate template = new SaveDataTemplate();
+                template._lastStandingPosition = playerStandingTransform.position;
+                template._lastStandingRotation = playerStandingTransform.rotation;
+                template._sceneName = sceneName;
+
+                string jsonStr = JsonUtility.ToJson(template);
+
+                StreamWriter sw = new StreamWriter(_playerDataPath, false);
+                sw.WriteLine(jsonStr);
+                sw.Flush();
+                sw.Close();
+            }
+        }
     }
 }
--- Assets/SLib_Pack/SLib/LibraryCore/GameSystem/SaveData/Scripts/PlayerSaveDataSerializer.cs
+++ Assets/SLib_Pack/SLib/LibraryCore/GameSystem/SaveData/Scripts/PlayerSaveDataSerializer.cs
@@ -3,23 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
-
-public class PlayerSaveDataSerializer : SingletonBaseClass<PlayerSaveDataSerializer> // セーブデータの展開
+using SLib.Systems;
+// 作成 菅沼
+namespace SLib
 {
-    protected override void ToDoAtAwakeSingleton() { }
-
-    public void ReadFuckingData()
+    namespace Systems
     {
-        var data = ReadSaveData();
-        Debug.Log($"{data._lastStandingTransform.position.ToString()}," +
-            $"{data._lastStandingTransform.rotation.ToString()} = {data._sceneName}");
-    }
+        public class PlayerSaveDataSerializer : SingletonBaseClass<PlayerSaveDataSerializer> // セーブデータの展開
+        {
+            protected override void ToDoAtAwakeSingleton() { }
 
-    public SaveDataTemplate ReadSaveData()
-    {
-        StreamReader sr = new StreamReader(Application.dataPath + "/PlayerSavedData.json");
-        string dataStr = sr.ReadToEnd();
-        sr.Close();
-        return JsonUtility.FromJson<SaveDataTemplate>(dataStr);
+            public SaveDataTemplate ReadSaveData()
+            {
+                StreamReader sr = new StreamReader(Application.dataPath + "/PlayerSavedData.json");
+                string dataStr = sr.ReadToEnd();
+                sr.Close();
+                return JsonUtility.FromJson<SaveDataTemplate>(dataStr);
+            }
+        }
+
     }
 }
